eecb914b742d7e563641ed9c892b810f1f9d65ee
parkoon <devparkoon@gmai.com>
2021-08-03 16:29:29 +0900

    Page & Router

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Header from "../header/header.component";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "../../common/styles";
 import "./app.component.css";
+import About from "../../pages/about/about.page";
+import Home from "../../pages/home/home.page";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Header />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
     </Router>
   );
 };
---
+++ src/pages/about/about.page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About: React.FC = () => {
+  return <div className="about">About Page</div>;
+};
+
+export default About;
