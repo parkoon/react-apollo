e4ca3c5d9629b6e49fe52f73e7ce805e10e1d8e2
parkoon <devparkoon@gmai.com>
2021-08-03 16:25:18 +0900

    Style

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "../header/header.component";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import "../../common/styles";
+import "./app.component.css";
+
 const App: React.FC = () => {
   return (
     <Router>
